bb8eacb03b0c53a403bfe98bc3d56277b143886f
GHOST <90078732+danuwrdna@users.noreply.github.com>
2023-12-05 16:52:26 +0700

    API
    API For Music

---
+++ animalswiki/animalswiki/Module/MusicViewController/Model/ColdplayApiModel.swift
@@ -0,0 +1,171 @@
+// This file was generated from JSON Schema using quicktype, do not modify it directly.
+// To parse the JSON, add this file to your project and do:
+//
+//   let welcome = try? JSONDecoder().decode(Welcome.self, from: jsonData)
+
+// This file was generated from JSON Schema using quicktype, do not modify it directly.
+// To parse the JSON, add this file to your project and do:
+//
+//   let welcome = try? JSONDecoder().decode(Welcome.self, from: jsonData)
+
+import Foundation
+
+// MARK: - Welcome
+struct Welcme: Codable {
+    let data: [Datum]?
+    let total: Int?
+    let next: String?
+}
+
+// MARK: - Datum
+struct Datum: Codable {
+    let id: Int?
+    let readable: Bool?
+    let title, titleShort: String?
+    let titleVersion: TitleVersion?
+    let link: String?
+    let duration, rank: Int?
+    let explicitLyrics: Bool?
+    let explicitContentLyrics, explicitContentCover: Int?
+    let preview: String?
+    let contributors: [Contributor]?
+    let md5Image: String?
+    let artist: Artist?
+    let album: Album?
+    let type: DatumType?
+    
+    enum CodingKeys: String, CodingKey {
+        case id, readable, title
+        case titleShort
+        case titleVersion
+        case link, duration, rank
+        case explicitLyrics
+        case explicitContentLyrics
+        case explicitContentCover
+        case preview, contributors
+        case md5Image
+        case artist, album, type
+    }
+}
+
+// MARK: - Album
+struct Album: Codable {
+    let id: Int?
+    let title: String?
+    let cover: String?
+    let coverSmall, coverMedium, coverBig, coverXl: String?
+    let md5Image: String?
+    let tracklist: String?
+    let type: AlbumType?
+    
+    enum CodingKeys: String, CodingKey {
+        case id, title, cover
+        case coverSmall
+        case coverMedium
+        case coverBig
+        case coverXl
+        case md5Image
+        case tracklist, type
+    }
+}
+
+enum AlbumType: String, Codable {
+    case album = "album"
+}
+
+// MARK: - Artist
+struct Artist: Codable {
+    let id: Int?
+    let name: Name?
+    let tracklist: String?
+    let type: ArtistType?
+}
+
+enum Name: String, Codable {
+    case bts = "BTS"
+    case coldplay = "Coldplay"
+    case rihanna = "Rihanna"
+    case selenaGomez = "Selena Gomez"
+    case theChainsmokers = "The Chainsmokers"
+}
+
+enum ArtistType: String, Codable {
+    case artist = "artist"
+}
+
+// MARK: - Contributor
+struct Contributor: Codable {
+    let id: Int?
+    let name: Name?
+    let link, share, picture: String?
+    let pictureSmall, pictureMedium, pictureBig, pictureXl: String?
+    let radio: Bool?
+    let tracklist: String?
+    let type: ArtistType?
+    let role: Role?
+    
+    enum CodingKeys: String, CodingKey {
+        case id, name, link, share, picture
+        case pictureSmall
+        case pictureMedium
+        case pictureBig
+        case pictureXl
+        case radio, tracklist, type, role
+    }
+}
+
+enum Role: String, Codable {
+    case main = "Main"
+}
+
+enum TitleVersion: String, Codable {
+    case acousticVersion = "(Acoustic Version)"
+    case empty = ""
+    case radioEdit = "(Radio Edit)"
+    case seebRemix = "(Seeb Remix)"
+}
+
+enum DatumType: String, Codable {
+    case track = "track"
+}
+
+class ColdplayApiModel{
+    func fetchData(completion: @escaping ([Datum]?) -> Void) {
+        guard let url = URL(string: "https://api.deezer.com/artist/892/top?limit=50") else {
+            completion(nil)
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data = data, error == nil else {
+                completion(nil)
+                return
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                let result = try decoder.decode(Welcme.self, from: data)
+                completion(result.data)
+            } catch {
+                print(error)
+                completion(nil)
+            }
+        }.resume()
+    }
+    func searchData(searchTerm: String, completion: @escaping ([Datum]?) -> Void) {
+        fetchData { [weak self] data in
+            guard let allData = data else {
+                completion(nil)
+                return
+            }
+            let filteredData = allData.filter { datum in
+                let isArtistMatch = datum.artist?.name?.rawValue.lowercased().contains(searchTerm.lowercased()) == true
+                let isTitleMatch = datum.title?.lowercased().contains(searchTerm.lowercased()) == true
+                return isArtistMatch || isTitleMatch
+            }
+            
+            completion(filteredData)
+        }
+    }
+    
+}
--- animalswiki/animalswiki/Module/MusicViewController/MusicViewController.swift
+++ animalswiki/animalswiki/Module/MusicViewController/MusicViewController.swift
@@ -1,7 +1,10 @@
 import UIKit
 import AVFoundation
+import Kingfisher
 class MusicViewController: UIViewController {
+    var model = ColdplayApiModel()
     var musicModel : [MusicViewModel] = []
+    var modelDatum: [Datum] = []
     var player: AVPlayer?
     var playerItem: AVPlayerItem?
     var timer: Timer?
@@ -13,9 +16,10 @@ class MusicViewController: UIViewController {
     @IBOutlet weak var subHeadLabel: UILabel!
     @IBOutlet weak var headLabel: UILabel!
     @IBOutlet weak var imgAB: UIImageView!
+    @IBOutlet weak var searchField: UITextField!
     @IBOutlet weak var progressBar: UIProgressView!
     @IBOutlet weak var tableMusic: UITableView!
-    
+    @IBOutlet weak var searchBT: UIButton!
     @IBAction func nextButton(_ sender: UIButton) {
         nextBT()
     }
@@ -30,11 +34,27 @@ class MusicViewController: UIViewController {
     @IBOutlet weak var viewHome: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        searchField.delegate = self
         delegateTable()
         musicModel = MusicViewModel.sampleData
         progress()
         playPause()
         border()
+        viewModel()
+    }
+    
+}
+extension MusicViewController{
+    func viewModel(){
+        let musicViewModel = ColdplayApiModel()
+        musicViewModel.fetchData { [weak self] data in
+            if let data = data {
+                self?.modelDatum = data
+                DispatchQueue.main.async {
+                    self?.tableMusic.reloadData()
+                }
+            }
+        }
     }
 }
 extension MusicViewController{
@@ -90,7 +110,7 @@ extension MusicViewController: UITableViewDelegate, UITableViewDataSource{
         tableMusic.registerCellWithNib(MusicTableViewCell.self)
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return musicModel.count
+        return modelDatum.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -100,10 +120,16 @@ extension MusicViewController: UITableViewDelegate, UITableViewDataSource{
     }
     
     func configureCell(_ cell: MusicTableViewCell, at indexPath: IndexPath) {
-        let msModel = musicModel[indexPath.row]
-        cell.titleLabel?.text = msModel.title
-        cell.subTitleLabel?.text = msModel.subTitle
-        cell.imgView?.image = UIImage(named: msModel.imageName)
+        let deezerTrack = modelDatum[indexPath.row]
+        cell.titleLabel?.text = deezerTrack.artist?.name?.rawValue
+        cell.subTitleLabel?.text = deezerTrack.title
+        if let imageUrl = deezerTrack.album?.cover {
+            let url = URL(string: imageUrl)
+            cell.imgView?.kf.setImage(with: url)
+        } else {
+            // Handle case when album cover URL is not available
+            cell.imgView?.image = UIImage(named: "placeholderImage") // You can use a placeholder image
+        }
     }
     func updateCell(at indexPath: IndexPath) {
         if let cell = tableMusic.cellForRow(at: indexPath) as? MusicTableViewCell {
@@ -112,6 +138,19 @@ extension MusicViewController: UITableViewDelegate, UITableViewDataSource{
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //        let deezerTrack = modelDatum[indexPath.row]
+        //        if let previewURLString = deezerTrack.preview,
+        //           let encodedURLString = previewURLString.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed),
+        //           let previewURL = URL(string: encodedURLString) {
+        //            let previewURLString = previewURL.absoluteString
+        //
+        //                // Pass the String to the playSnd function
+        //                playSnd(soundName: previewURLString, startTime: playbackTime)
+        //        } else {
+        //            // Handle case when preview URL is not available or cannot be converted to a valid URL
+        //        }
+        //
+        //    }
         playy()
     }
 }
@@ -134,33 +173,77 @@ extension MusicViewController {
             return
         }
         
-        let selectedSound = musicModel[selectedIndexPath.row]
-        headLabel.text = selectedSound.title
-        subHeadLabel.text = selectedSound.subTitle
-        imgAB.image = UIImage(named: selectedSound.imageName)
+        let selectedSound = modelDatum[selectedIndexPath.row]
+        headLabel.text = selectedSound.artist?.name?.rawValue
+        subHeadLabel.text = selectedSound.title
+        if let coverURLString = selectedSound.album?.cover,
+           let coverURL = URL(string: coverURLString) {
+            imgAB.kf.setImage(with: coverURL)
+        } else {
+            // Handle case when cover URL is not available
+            // You might want to show an alert or take appropriate action
+            imgAB.image = UIImage(named: "placeholderImage")
+        }
         
         
         if let player = player {
             if player.rate > 0 {
-                print("lgi di pause musiknya")
+                // Musik sedang diputar, jadi pause
+                print("Lagu sedang diputar, di-pause")
                 pauseMusic()
-                
             } else {
-                print("lgi di putar musiknya")
+                // Musik sedang di-pause, jadi putar
+                print("Lagu di-pause, dilanjutkan pemutaran")
                 resumeMusic()
-                playSnd(soundName: selectedSound.sound, startTime: playbackTime)
                 
+                // Periksa apakah ada lagu yang dipilih
+                if let selectedIndexPath = tableMusic.indexPathForSelectedRow {
+                    let selectedSound = modelDatum[selectedIndexPath.row]
+                    
+                    if let selectedPreview = selectedSound.preview, !selectedPreview.isEmpty {
+                        // Ada URL preview, putar lagu baru
+                        print("Memutar lagu baru")
+                        playSnd(soundName: selectedPreview, startTime: playbackTime)
+                    } else {
+                        // URL preview tidak tersedia
+                        print("URL preview tidak tersedia")
+                    }
+                } else {
+                    // Tidak ada lagu yang dipilih
+                    print("Tidak ada lagu yang dipilih")
+                }
             }
         } else {
-            print("nil")
-            
+            // Player nil, inisialisasi dan putar lagu yang sedang dipilih
+            print("Player nil, inisialisasi dan putar lagu yang sedang dipilih")
             resumeMusic()
-            playSnd(soundName: selectedSound.sound, startTime: playbackTime)
             
+            // Periksa apakah ada lagu yang dipilih
+            if let selectedIndexPath = tableMusic.indexPathForSelectedRow {
+                let selectedSound = modelDatum[selectedIndexPath.row]
+                
+                if let selectedPreview = selectedSound.preview, !selectedPreview.isEmpty {
+                    // Ada URL preview, putar lagu baru
+                    print("Memutar lagu baru")
+                    playSnd(soundName: selectedPreview, startTime: playbackTime)
+                } else {
+                    // URL preview tidak tersedia
+                    print("URL preview tidak tersedia")
+                }
+            } else {
+                // Tidak ada lagu yang dipilih
+                print("Tidak ada lagu yang dipilih")
+            }
         }
+        
+        // Setel status pemutaran
         isPlaying = player?.rate ?? 0 > 0
+        
+        // Reset state
         stateReset()
     }
+    
+    
     func playMusic() {
         player?.play()
         setPlayPauseImage(isPlaying: true)
@@ -192,8 +275,8 @@ extension MusicViewController {
     }
     
     func playSnd(soundName: String, startTime: Double) {
-        guard let url = Bundle.main.url(forResource: soundName, withExtension: "mp3") else {
-            print("Sound file not found.")
+        guard let url = URL(string: soundName) else {
+            print("Invalid URL.")
             return
         }
         playerItem = AVPlayerItem(url: url)
@@ -203,6 +286,7 @@ extension MusicViewController {
         player?.play()
         startTimer()
     }
+    
     func startTimer() {
         timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateProgress), userInfo: nil, repeats: true)
     }
@@ -225,17 +309,26 @@ extension MusicViewController {
         progressBar.progress = 0.0
         
     }
-    func stateReset(){
-        if let selectedIndexPath = tableMusic.indexPathForSelectedRow {
-            let selectedSound = musicModel[selectedIndexPath.row]
-            // Periksa apakah trek yang dipilih sama dengan yang sedang diputar
-            if let player = player, let playerItem = player.currentItem, let url = Bundle.main.url(forResource: selectedSound.sound, withExtension: "mp3"), let currentSoundURL = (playerItem.asset as? AVURLAsset)?.url {
-                if url == currentSoundURL {
-                    // Jika trek yang dipilih sama dengan yang sedang diputar, gunakan waktu saat ini
-                    playbackTime = player.currentTime().seconds
-                } else {
-                    // Jika trek yang dipilih berbeda, atur waktu putar ulang ke 0.0
-                    playbackTime = 0.0
+    func stateReset() {
+        guard let selectedIndexPath = tableMusic.indexPathForSelectedRow else {
+            playbackTime = 0.0
+            return
+        }
+        
+        let selectedSound = modelDatum[selectedIndexPath.row]
+        
+        if let player = player, let playerItem = player.currentItem {
+            if let currentSoundURL = (playerItem.asset as? AVURLAsset)?.url {
+                let selectedPreviewURLString = selectedSound.preview ?? ""
+                
+                if let selectedPreviewURL = URL(string: selectedPreviewURLString) {
+                    if selectedPreviewURL == currentSoundURL {
+                        // Jika trek yang dipilih sama dengan yang sedang diputar, gunakan waktu saat ini
+                        playbackTime = player.currentTime().seconds
+                    } else {
+                        // Jika trek yang dipilih berbeda, atur waktu putar ulang ke 0.0
+                        playbackTime = 0.0
+                    }
                 }
             } else {
                 // Jika tidak ada trek yang diputar, atur waktu putar ulang ke 0.0
@@ -243,6 +336,7 @@ extension MusicViewController {
             }
         }
     }
+    
     func formattedTime(time: Double) -> String {
         let minutes = Int(time) / 60
         let seconds = Int(time) % 60
@@ -274,21 +368,23 @@ extension MusicViewController {
     }
 }
 extension MusicViewController{
-    func nextBT(){
+    func nextBT() {
         guard let selectedIndexPath = tableMusic.indexPathForSelectedRow else {
             return
         }
+        
         var newIndexPath: IndexPath
-        if selectedIndexPath.row < musicModel.count - 1 {
+        if selectedIndexPath.row < modelDatum.count - 1 {
             newIndexPath = IndexPath(row: selectedIndexPath.row + 1, section: selectedIndexPath.section)
         } else {
             newIndexPath = IndexPath(row: 0, section: selectedIndexPath.section)
         }
-        tableMusic.deselectRow(at: selectedIndexPath, animated: true)
+        
         tableMusic.selectRow(at: newIndexPath, animated: true, scrollPosition: .none)
         tableView(tableMusic, didSelectRowAt: newIndexPath)
     }
-    func previousBT(){
+    
+    func previousBT() {
         guard let selectedIndexPath = tableMusic.indexPathForSelectedRow else {
             return
         }
@@ -297,12 +393,13 @@ extension MusicViewController{
         if selectedIndexPath.row > 0 {
             newIndexPath = IndexPath(row: selectedIndexPath.row - 1, section: selectedIndexPath.section)
         } else {
-            newIndexPath = IndexPath(row: musicModel.count - 1, section: selectedIndexPath.section)
+            newIndexPath = IndexPath(row: modelDatum.count - 1, section: selectedIndexPath.section)
         }
-        tableMusic.deselectRow(at: selectedIndexPath, animated: true)
+        
         tableMusic.selectRow(at: newIndexPath, animated: true, scrollPosition: .none)
         tableView(tableMusic, didSelectRowAt: newIndexPath)
     }
+    
 }
 
 extension MusicViewController{
@@ -318,4 +415,27 @@ extension MusicViewController{
         viewHome.layer.rasterizationScale = UIScreen.main.scale
         
     }
+}
+extension MusicViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        performSearch()
+        return true
+    }
+    func performSearch() {
+        guard let searchTerm = searchField.text?.trimmingCharacters(in: .whitespacesAndNewlines), !searchTerm.isEmpty else {
+            return
+        }
+        let musicViewModel = ColdplayApiModel()
+        model.searchData(searchTerm: searchTerm) { [weak self] data in
+            if let data = data as? [Datum] {
+                self?.modelDatum = data
+                DispatchQueue.main.async {
+                    self?.tableMusic.reloadData()
+                }
+            }
+        }
+        
+    }
+    
 }
