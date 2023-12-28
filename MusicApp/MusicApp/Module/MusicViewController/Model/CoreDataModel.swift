2702a11d2a4973fda5a621694ba0e505c737c8ef
GHOST <90078732+danuwrdna@users.noreply.github.com>
2023-12-28 09:38:29 +0700

    test
    tes

---
+++ MusicApp/MusicApp/Module/MusicViewController/Model/CoreDataModel.swift
@@ -0,0 +1,14 @@
+//
+//  CoreDataModel.swift
+//  MusicApp
+//
+//  Created by Phincon on 21/12/23.
+//
+
+import Foundation
+
+struct HistoryCoreDataModel {
+    var title: String
+    var subtitle: String
+    var image: String
+}
---
+++ MusicApp/MusicApp/Module/MusicViewController/cell/MusicTableViewCell.swift
@@ -0,0 +1,37 @@
+
+import UIKit
+
+class MusicTableViewCell: UITableViewCell {
+    @IBOutlet weak var imgView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var subTitleLabel: UILabel!
+    @IBOutlet weak var durationLabel: UILabel!
+    @IBOutlet weak var viewTB: UIView!
+    @IBOutlet weak var viewSW: UIView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        selectionStyle = .none
+        borderImg()
+        borderTable()
+    }
+}
+extension MusicTableViewCell{
+    func borderImg(){
+        imgView.layer.cornerRadius = 0
+        imgView.clipsToBounds = true
+
+    }
+    
+    func borderTable(){
+        viewSW.layer.cornerRadius = 16
+        viewSW.layer.shadowColor = UIColor.black.cgColor
+        viewSW.layer.shadowOpacity = 0.5
+        viewSW.layer.shadowOffset = CGSize(width: 0, height: 2)
+        viewSW.layer.shadowRadius = 4.0
+//        viewSW.layer.shouldRasterize = true
+        
+       viewSW.layer.masksToBounds = false
+       viewTB.layer.cornerRadius = 16
+       viewTB.layer.masksToBounds = true
+    }
+}
